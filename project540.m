f61eda8e28e01538588b6b1572343a674f35b244
austeane <austeane@gmail.com>
2016-03-26 22:41:43 -0700

    Standardized data, added grad decent function
    Added findMin and findMinL1, Mark’s grad decent functions
    project540 loads and standardizes the data
    added first model, logistic regression

--- project540.m
+++ project540.m
@@ -1,9 +1,33 @@
-clear
-load train.mat
-load test.mat
-X=train(:,1:end-1);
-Y=train(:,end);
-Xtest=test;
-[x,y]=find(X>1000000000);
-X(x,y)=0;
-max(max(X))
+% clear
+% load train.mat
+% load test.mat
+[n,d]=size(train(:,1:end-1));
+[nt,dt]=size(test);
+
+%%Combining for standardization
+Xc=[train(:,1:end-1);test];
+
+%%Standardizing and removing duplicates
+Xc=unique(Xc','rows','stable')';
+[Xc,mu,sigma] = standardizeCols(Xc);
+
+%%Seperating again
+X=Xc(1:n,:);
+Xtest=Xc(n+1:end,:);
+
+
+y=train(:,end);
+%Train model
+model = logRegL0(X,y,.5);
+
+%Sparsity of w
+numberOfNonZero = nnz(model.w)
+
+%Training error
+yhat = model.predict(model,X);
+trainingError = sum(yhat ~= y)/n
+
+
+
+%Predictions
+yhat = model.predict(model,Xtest);
